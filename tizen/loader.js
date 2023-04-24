25be2a8be538c1c7000a9f23aaca8c0a8da343b3
igrva <92253637+igrva@users.noreply.github.com>
2023-04-24 10:28:35 +0300

    Update loader.js

--- tizen/loader.js
+++ tizen/loader.js
@@ -1,1 +1,14 @@
-host="http://ott.drm-play.com";
+var host='http://ott.drm-play.com';
+(function() {
+    // Load the script
+    var script = document.createElement("SCRIPT");
+    script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
+    script.type = 'text/javascript';
+    script.onload = function(){
+        $(document.body).load(host+'/stbPlayer/stbPlayer_body.html');
+        $.getScript(host+'/stbPlayer/stbPlayer.js');
+        $.getScript(host+'/samsung/tizen/stb.js');
+    };
+    document.getElementsByTagName("head")[0].appendChild(script);
+})();
+
